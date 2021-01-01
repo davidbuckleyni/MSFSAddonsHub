a3a6c890a4fe90da9aafe9c18b63e454375a5877
david buckley <davidbuckleyweb@gmail.com>
2021-01-01 03:06:12 +0000

    First commint of app work

--- MSFSAddonsHub.Forms/MSFSAddonsHub.Forms/MSFSAddonsHub.Forms/AppShell.xaml.cs
+++ MSFSAddonsHub.Forms/MSFSAddonsHub.Forms/MSFSAddonsHub.Forms/AppShell.xaml.cs
@@ -11,13 +11,15 @@ namespace MSFSAddonsHub.Forms
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(MainMenu), typeof(MainMenu));
+
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
         {
-            await Shell.Current.GoToAsync("//LoginPage");
+            await Shell.Current.GoToAsync("//MainMenu");
         }
     }
 }
