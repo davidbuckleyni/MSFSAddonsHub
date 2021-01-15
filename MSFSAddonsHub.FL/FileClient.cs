717fa616b908f5959aac2d88591fad678db6da51
david buckley <davidbuckleyweb@gmail.com>
2021-01-15 16:26:38 +0000

    Uisng mega for file uploads

---
+++ MSFSAddonsHub.FL/FileClient.cs
@@ -0,0 +1,32 @@
+﻿using CG.Web.MegaApiClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MSFSAddonsHub.FL
+{
+    public class FileClient
+    {
+
+
+
+        public void UploadFile(string fileName)
+
+        {
+            var client = new MegaApiClient();
+            client.Login("username@domain.com", "passw0rd");
+
+            IEnumerable<INode> nodes = client.GetNodes();
+
+            INode root = nodes.Single(x => x.Type == NodeType.Root);
+            INode myFolder = client.CreateFolder("Upload", root);
+
+            INode myFile = client.UploadFile(filename, myFolder);
+            Uri downloadLink = client.GetDownloadLink(myFile);
+            Console.WriteLine(downloadLink);
+
+            client.Logout();
+
+        }
+    }
+}
