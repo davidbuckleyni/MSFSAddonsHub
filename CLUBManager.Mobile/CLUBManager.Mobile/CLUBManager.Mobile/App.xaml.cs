21299d5fe8f8b340c705c82326c609ef41d0b3f4
david buckley <davidbuckleyweb@gmail.com>
2021-03-21 15:25:17 +0000

    Addded mobile application

---
+++ CLUBManager.Mobile/CLUBManager.Mobile/CLUBManager.Mobile/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using CLUBManager.Mobile.Services;
+using CLUBManager.Mobile.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CLUBManager.Mobile
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ CLUBManager.Mobile/CLUBManager.Mobile/CLUBManager.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CLUBManager.Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CLUBManager.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
